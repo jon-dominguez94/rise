8275d2f5c787185c05824b31964ca5365527e798
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-28 20:18:02 -0800

    Connect app to mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => res.send("Helladqo World"));
 
 const port = process.env.PORT || 5000;
