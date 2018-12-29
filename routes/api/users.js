ac1bdac7d947c7073cd69d1f6e1d2ff7b48f1d94
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-29 08:48:11 -0800

    Add web token to login

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,7 +45,19 @@ router.post('/login', (req, res) => {
     bcrypt.compare(password, user.password)
     .then(isMatch => {
       if(isMatch){
-        const payload = { id: user.id, email: user.email}
+        const payload = { id: user.id, email: user.email };
+
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: 'Incorrect password' });
       }
