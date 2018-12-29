339fcb7b0ebe61520fb0431eaf14e9e1dbdcd9d1
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-29 02:18:52 -0800

    Add username to user model

--- models/User.js
+++ models/User.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true
