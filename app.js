3f1a8d68352ee3851e74ffd2cc54aa399c8ef3c5
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-29 02:17:50 -0800

    Define user model

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 
+const users = require('./routes/api/users');
+
+const bodyParser = require('body-parser');
+
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log('Connected to MongoDB successfully'))
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err));
 
-const users = require('./routes/api/users');
-app.use("/api/users", users);
 
-app.get("/", (req, res) => res.send("Helladqo World"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
