67c286d24f840886ae2a20100aca4a47b11c7565
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-29 02:34:23 -0800

    Define login user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -27,8 +27,28 @@ router.post('/register', (req, res) => {
         });
       });
     }
-  })
-  .catch(err => console.log(err));
+  });
+});
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+  .then(user => {
+    if(!user){
+      return res.status(404).json({ email: 'This user does not exist' });
+    }
+
+    bcrypt.compare(password, user.password)
+    .then(isMatch => {
+      if(isMatch){
+        res.json({ msg: 'Success' });
+      } else {
+        return res.status(400).json({ password: 'Incorrect password' });
+      }
+    });
+  });
 });
 
 module.exports = router;
