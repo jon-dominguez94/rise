82c40972cb5c4bdb54c5e9c21dad11399d67e013
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-28 20:08:35 -0800

    Add server scripts

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Helladqo World"));
 
 const port = process.env.PORT || 5000;
 
