8a7c038dca7d8987983929f205a398aef6fe864a
TedWildenradt <tedkwildenradt@gmail.com>
2019-01-03 10:36:27 -0800

    Setup sms messaging function

--- aws-sms.js
+++ aws-sms.js
@@ -1,5 +1,11 @@
 var AWS = require('aws-sdk');
-AWS.config.region = 'us-west-2';
+require('dotenv').config()
+// AWS.config.region = 'us-west-2';
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
 var sns = new AWS.SNS();
 
 var params = {
