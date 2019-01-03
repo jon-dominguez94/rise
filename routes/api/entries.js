918b6ffb062677f02cd0498c56e229c879a9b8ff
Sameeh <sameehkhan1@gmail.com>
2019-01-03 10:42:59 -0800

    Import entry model

--- routes/api/entries.js
+++ routes/api/entries.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+const Entry = require('../../models/Entry');
+
+
 router.get("/test", (req, res) => res.json({ msg: "This is the entry test route" }));
 
 module.exports = router;
