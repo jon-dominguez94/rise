85b8937e30117497c72ec1b27072d526046d3cc9
Sameeh <sameehkhan1@gmail.com>
2019-01-03 09:41:54 -0800

    Successfully test entries route

--- routes/api/entries.js
+++ routes/api/entries.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/entrytest", (req, res) => res.json({ msg: "This is the entries test route" }));
+router.get("/test", (req, res) => res.json({ msg: "This is the entry test route" }));
 
 module.exports = router;
