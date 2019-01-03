84d9784267046b0dea4b15fd409d164a0324009d
Sameeh <sameehkhan1@gmail.com>
2019-01-03 10:19:24 -0800

    Export entry model

--- models/Entry.js
+++ models/Entry.js
@@ -1,2 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const EntrySchema = new Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    importance: {
+        type: String,
+        required: true
+    },
+    // goal_id: {
+    //     type: Integer,
+    //     required: true
+    // },
+    // role_id: {
+    //     type: Integer,
+    //     required: true
+    // },
+    // report_id: {
+    //     type: Integer,
+    //     required: true
+    // },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = User = mongoose.model('entries', UserSchema);
