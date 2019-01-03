4e94ecc67c92a4314c4110d297965e18395e4a10
TedWildenradt <tedkwildenradt@gmail.com>
2019-01-03 12:33:51 -0800

    fix host error

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
