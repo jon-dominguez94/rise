970361223a0845014a3038f8cde5fb50fea2f015
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-03 09:40:27 -0800

    Create profile page component

---
+++ frontend/src/components/pages/profile_page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ProfilePage extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return(
+      <div></div>
+    );
+  }
+}
+
+export default ProfilePage;
---
+++ frontend/src/components/pages/profile_page_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import ProfilePage from './profile_page';
+
+const mstp = state => {
+  return {
+
+  };
+};
+
+const mdtp = dispatch => {
+  return {
+
+  };
+};
+
+export default connect(mstp, mdtp)(ProfilePage);
