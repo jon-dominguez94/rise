6d6274efdbf05973e99c3fe2a6a9b44727123809
Sameeh <sameehkhan1@gmail.com>
2019-01-03 10:42:29 -0800

    Import router into entries route

--- routes/api/entries.js
+++ routes/api/entries.js
@@ -1,5 +1,7 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the entry test route" }));
 
