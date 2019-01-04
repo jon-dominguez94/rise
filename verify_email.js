c606030abd5e7ab2cce839ad592a45a16ccefcee
Mark Kopec <mark.kopec@gmail.com>
2019-01-04 11:17:24 -0800

    Add custom verification email

--- verify_email.js
+++ verify_email.js
@@ -6,9 +6,11 @@ const ses = new AWS.SES();
 
 
 var params = {
-  EmailAddress: "mark.kopec@gmail.com"
+  EmailAddress: "mark.kopec@gmail.com",
+  TemplateName: "RiseEmailTemplate"
 };
-ses.verifyEmailAddress(params, function (err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else console.log(data);           // successful response
+ses.sendCustomVerificationEmail(params, function(err, data) {
+  if (err) console.log(err, err.stack);
+  // an error occurred
+  else console.log(data); // successful response
 });
