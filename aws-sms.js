294331d630faf1fd65d9d1feb5540cda822847e2
TedWildenradt <tedkwildenradt@gmail.com>
2019-01-04 11:15:19 -0800

    Setup node-scheduler

--- aws-sms.js
+++ aws-sms.js
@@ -1,6 +1,7 @@
 var AWS = require('aws-sdk');
+var schedule = require('node-schedule');
 require('dotenv').config()
-// AWS.config.region = 'us-west-2';
+
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -9,7 +10,7 @@ AWS.config.update({
 var sns = new AWS.SNS();
 
 var params = {
-  Message: 'Rise: Your career is calling!',
+  Message: 'Time to update your profile on Rise!',
   MessageStructure: 'string',
   PhoneNumber: '+18313453689'
 };
@@ -17,4 +18,17 @@ var params = {
 sns.publish(params, function(err, data) {
   if (err) console.log(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
+});
+
+// // Weekly timer
+// const weeklyInterval = setInterval(reminderFunc, 604800000)
+
+// // Daily timer
+// const dailyInterval = setInterval(reminderFunc, 86400000)
+
+// // BiWeekly timer
+// const BiWeeklyIntervale = setInterval(reminderFunc, 1209600000)
+
+var j = schedule.scheduleJob(`* * ${hour} * ${day}`, function(){
+  console.log('The answer to life, the universe, and everything!');
 });
