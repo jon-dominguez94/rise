134af9f95ad974afddb6360b6c0d32b9eaf3e79c
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-05 09:19:28 -0800

    Get profile update to work properly

--- routes/api/users.js
+++ routes/api/users.js
@@ -72,25 +72,28 @@ router.patch('/profile', passport.authenticate('jwt', { session: false }), (req,
       if (!user) {
         return res.status(404).json({ email: 'This user does not exist' });
       }
-
+      console.log(user);
       const oldPw = user.password;
       user.fname = validText(req.body.fname) ? req.body.fname : user.fname;
       user.lname = validText(req.body.lname) ? req.body.lname : user.lname;
       user.password = validText(req.body.password) ? req.body.password : user.password;
       user.phone = validText(req.body.phone) ? req.body.phone : user.phone;
-
+      
+      console.log(user);
       if(user.password !== oldPw){
         // console.log('diff passwords');
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(user.password, salt, (err, hash) => {
             if (err) throw err;
             user.password = hash;
+            console.log(user);
             user.save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
           });
         });
       } else {
+        console.log(user);
         // console.log('same passwords');
         user.save()
           .then(user => res.json(user))
