89662defe451c2d60378e3ca7c3ab4079d068cbc
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-04 16:01:02 -0800

    Add error checks to profile route

--- routes/api/users.js
+++ routes/api/users.js
@@ -59,8 +59,12 @@ router.post('/register', (req, res) => {
 });
 
 router.patch('/profile', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const email = req.body.email;
+  const { errors, isValid } = validateRegisterInput(req.body);
 
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  
   User.findOne({ email: req.body.email })
     .then(user => {
 
