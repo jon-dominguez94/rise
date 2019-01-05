92fb8e8e8743a4dbfa0f66b42e2a214d54a9184c
TedWildenradt <tedkwildenradt@gmail.com>
2019-01-05 11:53:41 -0800

    Add day of week dropdown

--- frontend/src/components/reminder/reminder.jsx
+++ frontend/src/components/reminder/reminder.jsx
@@ -5,25 +5,44 @@ class Reminder extends React.Component {
     super(props);
 
     this.state = {
-
+      dayOfWeek: '',
+      hour: '',
+      minute: '',
+      emailReminder: '',
+      smsReminder: ''
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.renderErrors = this.renderErrors.bind(this);
   }
 
   handleSubmit(){
 
   }
 
-  renderErrors(){
-
+  handleUpdate(){
+    
   }
+
 
   render(){
     return(
       <div>
+        <select 
+        value={this.state.dayOfWeek}
+        onChange={this.handleUpdate}
+        className="day-of-week-selector">
+          <option value={0}>Sunday</option>
+          <option value={1}>Monday</option>
+          <option value={2}>Tuesday</option>
+          <option value={3}>Wednesday</option>
+          <option value={4}>Thursday</option>
+          <option selected value={5}>Friday</option>
+          <option value={6}>Saturday</option>
+        </select>
 
+        <select className="hour-selector">
+
+        </select>
       </div>
     )
   }
