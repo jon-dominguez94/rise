299a4ca3364b502e7a60648fc2912e176322a313
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-04 16:04:40 -0800

    Update update validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,7 @@ const keys = require('../../config/keys');
 const User = require('../../models/User');
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
+const validateUpdateInput = require('../../validation/update');
 const validText = require('../../validation/valid-text');
 
 router.get('/test', (req, res) => res.json({msg: "This is the users route"}));
@@ -59,12 +60,12 @@ router.post('/register', (req, res) => {
 });
 
 router.patch('/profile', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body);
+  const { errors, isValid } = validateUpdateInput(req.body);
 
   if (!isValid) {
     return res.status(400).json(errors);
   }
-  
+
   User.findOne({ email: req.body.email })
     .then(user => {
 
