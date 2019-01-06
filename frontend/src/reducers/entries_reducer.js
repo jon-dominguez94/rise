6ac6261825ebe2beb463e07183ba0d390cb870f9
Sameeh <sameehkhan1@gmail.com>
2019-01-05 16:56:57 -0800

    Freeze state in entries reducer

--- frontend/src/reducers/entries_reducer.js
+++ frontend/src/reducers/entries_reducer.js
@@ -1,8 +1,8 @@
 import { RECEIVE_REPORT_ENTRIES, RECEIVE_NEW_ENTRY } from '../actions/entry_actions';
 
 const EntriesReducer = (state = { report: {}, new: undefined }, action) => {
-
-
+    Object.freeze(state);
+    letnewState = Object.assign({}, state);
 
 };
 
