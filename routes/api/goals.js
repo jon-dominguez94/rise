baa49f2782a5de8992c8542dc4dc957ba1a4c1cd
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-05 16:54:51 -0800

    Add goals routes to app

--- routes/api/goals.js
+++ routes/api/goals.js
@@ -1,4 +1,4 @@
-const express = require('exporess');
+const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
@@ -6,11 +6,17 @@ const passport = require('passport');
 const Goal = require('../../models/Goal');
 const validateGoalInput = require('../../validation/goals');
 
-router.get('/user/:user_id', (req, res) => {
-  Goal.find({user: req.params.user_id})
-  .then(goals => res.json(goals))
-  .catch(err => res.status(404).json({ nogoalsfound: 'No goals found'}));
-});
+router.get("/test", (req, res) => res.json({ msg: "This is the goals route" }));
+
+router.get(
+  "/:user_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Goal.find({ user: req.params.user_id })
+      .then(goals => res.json(goals))
+      .catch(err => res.status(404).json({ nogoalsfound: "No goals found" }));
+  }
+);
 
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
   const { errors, isValid } = validateGoalInput(req.body);
