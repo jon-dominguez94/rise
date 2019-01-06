b26ecc79aa98b41e0bd811ec19b60ab0843d38f6
TedWildenradt <tedkwildenradt@gmail.com>
2019-01-06 13:39:21 -0800

    Fix env pathway

--- frontend/src/util/send_text_api.js
+++ frontend/src/util/send_text_api.js
@@ -1,6 +1,6 @@
 var AWS = require('aws-sdk');
 var schedule = require('node-schedule');
-require('dotenv').config()
+require('dotenv').config({path: '/Users/tckw00/Desktop/rise/frontend/.env'})
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -11,31 +11,56 @@ var sns = new AWS.SNS();
 
 // example params
 var params = {
-  Message: 'Time to update your achievements on Rise!',
+  Message: 'Time to update your achievements on Rise! 37',
   MessageStructure: 'string',
   PhoneNumber: '+18313453689'
 };
 
-//This sends the text message via aws SNS
-sns.publish(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
+// sns.publish(params, function(err, data) {
+//   if (err) console.log(err, err.stack); // an error occurred
+//   else     console.log(data);           // successful response
+// });
 
+// // Weekly timer
+// const weeklyInterval = setInterval(reminderFunc, 604800000)
+
+// // Daily timer
+// const dailyInterval = setInterval(reminderFunc, 86400000)
+
+// // BiWeekly timer
+// const BiWeeklyIntervale = setInterval(reminderFunc, 1209600000)
 
 var rule = new schedule.RecurrenceRule();
+// rule.dayOfWeek = 0
+// rule.hour = 13;
+rule.minute = 37;
 
-// example of rules for Saturday at 3:40pm
-rule.dayOfWeek = 7
-rule.hour = 12;
-rule.minute = 48;
+const timedText = () => (sns.publish(params));
  
-//this part sends messages on a recurring schedule
-var j = schedule.scheduleJob(rule, function(){
-  console.log('The scheduler worked');
-});
-// var j = schedule.scheduleJob(rule, sns.publish(params).bind(this));
+// var j = schedule.scheduleJob(rule, function(){
 
-// j();
-// Cancels the scheduled messages
+//   console.log('Today is recognized by Rebecca Black!');
+// });
+
+var j = schedule.scheduleJob(rule, function(){
+
+  AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+  });
+  var sns = new AWS.SNS();
+
+  sns.publish(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  })
+
+  console.log('complete')
+});
+
 // j.cancel();
+
+// var j = schedule.scheduleJob(`* * ${hour} * ${day}`, function(){
+//   console.log('The answer to life, the universe, and everything!');
+// });
