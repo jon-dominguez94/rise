16e9916f82378d85c80245139c6c2b9bb608bf35
Sameeh <sameehkhan1@gmail.com>
2019-01-05 18:12:39 -0800

    Create dropdown for entry rating

--- frontend/src/components/pages/entries/new_entry.jsx
+++ frontend/src/components/pages/entries/new_entry.jsx
@@ -25,6 +25,15 @@ class NewEntry extends React.Component{
                         value={this.state.description}
                         placeholder="Write description"
                     />
+                    <label>Rating
+                    <select>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                    </label>
                 </form>
             </div>
         )
