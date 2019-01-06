d2923548f1f778cebefef3ebfb7bd00644ddfcb8
Sameeh <sameehkhan1@gmail.com>
2019-01-05 17:45:12 -0800

    Pass in new entry slice of state to state props function

--- frontend/src/components/pages/entries/new_entries_container.js
+++ frontend/src/components/pages/entries/new_entries_container.js
@@ -1,2 +1,10 @@
 import { connect } from 'react-redux';
 import { createEntry } from '../../../actions/entry_actions';
+
+
+const mapStateToProps = (state) => {
+    return {
+        // userId: state.session.user.userId,
+        newEntry: state.entries.new
+    };
+};
