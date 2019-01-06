72a39e031364f8da8b80a2071a0f0e9925a265dc
Sameeh <sameehkhan1@gmail.com>
2019-01-05 16:57:48 -0800

    Return default in switch statement if no case matches

--- frontend/src/reducers/entries_reducer.js
+++ frontend/src/reducers/entries_reducer.js
@@ -3,7 +3,11 @@ import { RECEIVE_REPORT_ENTRIES, RECEIVE_NEW_ENTRY } from '../actions/entry_acti
 const EntriesReducer = (state = { report: {}, new: undefined }, action) => {
     Object.freeze(state);
     letnewState = Object.assign({}, state);
+    switch(action.type){
 
+        default: 
+            return state;
+    }
 };
 
 
