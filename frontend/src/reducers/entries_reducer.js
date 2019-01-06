3ff5df880bfe59a20e627fb989650cac7a7d97b3
Sameeh <sameehkhan1@gmail.com>
2019-01-05 16:58:56 -0800

    Add case for new entry

--- frontend/src/reducers/entries_reducer.js
+++ frontend/src/reducers/entries_reducer.js
@@ -2,9 +2,11 @@ import { RECEIVE_REPORT_ENTRIES, RECEIVE_NEW_ENTRY } from '../actions/entry_acti
 
 const EntriesReducer = (state = { report: {}, new: undefined }, action) => {
     Object.freeze(state);
-    letnewState = Object.assign({}, state);
+    let newState = Object.assign({}, state);
     switch(action.type){
-
+        case RECEIVE_NEW_ENTRY:
+            newState.new = action.entry.data
+            return newState;
         default: 
             return state;
     }
