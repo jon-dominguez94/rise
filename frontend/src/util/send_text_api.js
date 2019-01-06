4a6a93bc7b97625873906727a84d92be05733910
TedWildenradt <tedkwildenradt@gmail.com>
2019-01-06 12:05:06 -0800

    Adjusting text scheduler

--- frontend/src/util/send_text_api.js
+++ frontend/src/util/send_text_api.js
@@ -7,17 +7,20 @@ AWS.config.update({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION
 });
+
+AWS.config.update({ region: 'us-west-2' });
 var sns = new AWS.SNS();
 
 // example params
 var params = {
   Message: 'Time to update your achievements on Rise!',
   MessageStructure: 'string',
-  PhoneNumber: '+15106038483'
+  // PhoneNumber: '+15106038483'
+  PhoneNumber: '+18313453689'
 };
 
 //This sends the text message via aws SNS
-const sendText = sns.publish(params, function(err, data) {
+sns.publish(params, function(err, data) {
   if (err) console.log(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
 });
@@ -26,15 +29,15 @@ const sendText = sns.publish(params, function(err, data) {
 var rule = new schedule.RecurrenceRule();
 
 // example of rules for Saturday at 3:40pm
-rule.dayOfWeek = 6
-rule.hour = 16;
-rule.minute = 40;
+rule.dayOfWeek = 7
+rule.hour = 12;
+rule.minute = 48;
  
 //this part sends messages on a recurring schedule
 // var j = schedule.scheduleJob(rule, function(){
 //   console.log('The scheduler worked');
 // });
-var j = schedule.scheduleJob(rule, sns.publish(params));
+// var j = schedule.scheduleJob(rule, sns.publish(params).bind(this));
 
 // j();
 // Cancels the scheduled messages
