9400ee225a493bf19ee91d038ddd303a918ecd02
Sameeh <sameehkhan1@gmail.com>
2019-01-05 16:36:05 -0800

    Export entry action constants

--- frontend/src/actions/entry_actions.js
+++ frontend/src/actions/entry_actions.js
@@ -1,1 +1,4 @@
 import { writeEntry, getReportEntries } from '../util/entry_api_util';
+
+export const RECEIVE_NEW_ENTRY = "RECEIVE_NEW_ENTRY";
+export const RECEIVE_REPORT_ENTRIES = "RECEIVE_REPORT_ENTRIES";
