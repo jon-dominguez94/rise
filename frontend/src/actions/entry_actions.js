adb9a47215701d7f0f6e0ef06b705c804be486cd
Sameeh <sameehkhan1@gmail.com>
2019-01-05 16:39:45 -0800

    Add new entry and receive report entry functions

--- frontend/src/actions/entry_actions.js
+++ frontend/src/actions/entry_actions.js
@@ -2,3 +2,13 @@ import { writeEntry, getReportEntries } from '../util/entry_api_util';
 
 export const RECEIVE_NEW_ENTRY = "RECEIVE_NEW_ENTRY";
 export const RECEIVE_REPORT_ENTRIES = "RECEIVE_REPORT_ENTRIES";
+
+export const receiveReportEntries = entries => ({
+    type: RECEIVE_REPORT_ENTRIES,
+    entries
+});
+
+export const receiveNewEntry = ENTRY => ({
+    type: RECEIVE_NEW_ENTRY,
+    ENTRY
+});
