6b0fd1edaaecaa9fdf72331f548156064c2c4003
Mark Kopec <mark.kopec@gmail.com>
2019-01-06 11:54:10 -0800

    Insert script into class and export

--- frontend/src/util/email/email.js
+++ frontend/src/util/email/email.js
@@ -1,35 +1,53 @@
 require("dotenv").config();
 
-const AWS = require("aws-sdk");
 
-const ses = new AWS.SES();
+class AWSEmail {
 
-const params = {
-  Destination: {
-    ToAddresses: ["mark.kopec@gmail.com"]
-  },
-  Message: {
-    Body: {
-      Html: {
-        Charset: "UTF-8",
-        Data:
-          'This message body contains HTML formatting, like <a class="ulink" href="http://docs.aws.amazon.com/ses/latest/DeveloperGuide" target="_blank">Amazon SES Developer Guide</a>.'
+  constructor(emailAddress) {
+    this.emailAddress = emailAddress;
+  }
+
+  sendEmail() {
+    const AWS = require("aws-sdk");
+    AWS.config.update({ region: "us-west-2" });
+    const ses = new AWS.SES();
+
+    const params = {
+      Destination: {
+        ToAddresses: [this.emailAddress]
       },
-      Text: {
-        Charset: "UTF-8",
-        Data: "This is the message body in text format."
-      }
-    },
-    Subject: {
-      Charset: "UTF-8",
-      Data: "Time to manage your career"
-    }
-  },
-  ReturnPath: "timetorisenotification@gmail.com",
-  Source: "timetorisenotification@gmail.com"
-};
+      Message: {
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data:
+              'This is your scheduled reminder to enter your accomplishments in your report at Rise! <a class="ulink" href="https://time-to-rise.herokuapp.com" target="_blank">Rise Login</a>.'
+          },
+          Text: {
+            Charset: "UTF-8",
+            Data:
+              "This is your scheduled reminder to enter your accomplishments in your report at Rise!"
+          }
+        },
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Time to manage your career"
+        }
+      },
+      ReturnPath: "timetorisenotification@gmail.com",
+      Source: "timetorisenotification@gmail.com"
+    };
 
-ses.sendEmail(params, (err, data) => {
-  if (err) console.log(err, err.stack);
-  else console.log(data);
-});
+    ses.sendEmail(params, (err, data) => {
+      if (err) console.log(err, err.stack);
+      else console.log(data);
+    });
+  }
+
+}
+
+module.exports = AWSEmail;
+
+const Email = require('./email');
+const email = new Email("mark.kopec@gmail.com")
+email.sendEmail();
