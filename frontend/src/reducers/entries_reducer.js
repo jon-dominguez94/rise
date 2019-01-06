5cda4b34e81841ca703bc0578e408dfcaaed3121
Sameeh <sameehkhan1@gmail.com>
2019-01-05 16:54:40 -0800

    Create skeleton for entries reducer

--- frontend/src/reducers/entries_reducer.js
+++ frontend/src/reducers/entries_reducer.js
@@ -1,2 +1,10 @@
 import { RECEIVE_REPORT_ENTRIES, RECEIVE_NEW_ENTRY } from '../actions/entry_actions';
 
+const EntriesReducer = (state = { report: {}, new: undefined }, action) => {
+
+
+
+};
+
+
+export default EntriesReducer;
