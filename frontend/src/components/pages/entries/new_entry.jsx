1101f59b2ede6eeb87fc58499fcb8dff496fd06d
Sameeh <sameehkhan1@gmail.com>
2019-01-05 17:56:19 -0800

    Add blank div to render on new entry

--- frontend/src/components/pages/entries/new_entry.jsx
+++ frontend/src/components/pages/entries/new_entry.jsx
@@ -1,1 +1,24 @@
 import react from 'React';
+
+
+class NewEntry extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            description: "",
+            importance: ""
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                New Entry
+            </div>
+        )
+    }
+
+}
+
+export default NewEntry
