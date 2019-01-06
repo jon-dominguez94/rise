e7f6ced4e319c40c479c95e6da30ad6732357608
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-05 16:22:16 -0800

    Finish goal routes

--- routes/api/goals.js
+++ routes/api/goals.js
@@ -11,3 +11,22 @@ router.get('/user/:user_id', (req, res) => {
   .then(goals => res.json(goals))
   .catch(err => res.status(404).json({ nogoalsfound: 'No goals found'}));
 });
+
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validateGoalInput(req.body);
+
+  if(!isValid){
+    return res.status(400).json(errors);
+  }
+
+  const newGoal = new Goal({
+    title: req.body.title,
+    description: req.body.description,
+    user: req.user.id
+  });
+
+  newGoal.save()
+  .then(goal => res.json(goal));
+});
+
+module.exports = router;
