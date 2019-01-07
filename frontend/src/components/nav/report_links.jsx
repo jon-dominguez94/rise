3f6f5a3d7c9a0219699ab4580a29151c3c61b9b4
Ben Cowden <benjcowden@gmail.com>
2019-01-06 18:21:16 -0800

    Add state to ReportLinks

--- frontend/src/components/nav/report_links.jsx
+++ frontend/src/components/nav/report_links.jsx
@@ -5,30 +5,29 @@ class ReportLinks extends React.Component {
   constructor(props) {
     super(props);
 
-    // this.state = {
-    //   reports: []
-    // };
+    this.state = {
+      reports: []
+    };
   }
 
   componentWillMount(){
     this.props.fetchUserReports(this.props.user.id);
   }
 
-  // componentWillReceiveProps(newState){
-  //   console.log(newState);
-  //   this.setState({
-  //     reports: newState.reports
-  //   });
-  // }
+  componentWillReceiveProps(newState){
+    console.log(newState);
+    this.setState({
+      reports: newState.reports
+    });
+  }
 
   render() {
-    // debugger
     return (
       <div className="navbar-links">
         <span className="bar-link user-greet">Hello, {this.props.user.fname}!</span>
         <hr />
         {this.props.reports.map(report => (
-          <SingleReportLink key={report.id} report={report}/>
+          <SingleReportLink key={report._id} report={report}/>
         ))}
       </div>
     )
