9e145c29e098f20118abc3c3bc5add3151f53a78
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-07 04:58:50 -0800

    Create project errors reducer

--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import SessionErrorsReducer from './session_errors_reducer';
 import GoalErrorsReducer from './goal_errors_reducer';
 import RoleErrorsReducer from './role_errors_reducer';
+import ProjectErrorsReducer from './project_errors_reducer';
 
 export default combineReducers({
   session: SessionErrorsReducer,
   goals: GoalErrorsReducer,
-  roles: RoleErrorsReducer
+  roles: RoleErrorsReducer,
+  projects: ProjectErrorsReducer
 });
