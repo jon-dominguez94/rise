0c044caa7060579ef3b72db34703243b92e5eab2
Ben Cowden <benjcowden@gmail.com>
2019-01-07 09:52:19 -0800

    Nest reports under their id's

--- routes/api/reports.js
+++ routes/api/reports.js
@@ -10,12 +10,36 @@ router.get("/test", (req, res) => res.json({ msg: "This is the reports route" })
 router.get('/user/:user_id', (req, res) => {
   Report.find({ user: req.params.user_id })
     .sort({ date: -1 })
-    .then(reports => res.json(reports))
+    .then(reports => {
+      reportsObject = {};
+      reports.forEach(report => {
+        reportsObject[report._id] = report;
+      });
+      res.json(reportsObject);
+    })
     .catch(err =>
       res.status(404).json({ noreportsfound: 'No reports found from that user' }
       )
     );
 });
+
+// router.get(
+//   "/user/:user_id",
+//   passport.authenticate("jwt", { session: false }),
+//   (req, res) => {
+//     Goal.find({ user: req.params.user_id })
+//       .sort({ date: -1 })
+//       .then(goals => {
+//         goalsObject = {};
+//         goals.forEach(goal => {
+//           goalsObject[goal._id] = goal;
+//         });
+//         res.json(goalsObject);
+//         // res.json(goals);
+//       })
+//       .catch(err => res.status(404).json({ nogoalsfound: "No goals found" }));
+//   }
+// );
 
 router.get('/:id', (req, res) => {
   Report.findById(req.params.id)
