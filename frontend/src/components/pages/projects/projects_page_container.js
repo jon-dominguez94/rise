ad519c680743ddc81e74c73403e4fed9723bd56f
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-07 09:57:10 -0800

    Refactor projects component

--- frontend/src/components/pages/projects/projects_page_container.js
+++ frontend/src/components/pages/projects/projects_page_container.js
@@ -1,21 +1,23 @@
 import { connect } from 'react-redux';
 import { fetchUserProjects, updateProject, composeProject } from '../../../actions/project_actions';
 import ProjectsPage from './projects_page';
+import ProfileElement from "../profile/profile_element";
 
 const mstp = state => {
   return {
     user: state.session.user,
-    projects: Object.values(state.projects),
-    errors: state.errors.projects
+    elements: Object.values(state.projects),
+    errors: state.errors.projects,
+    label: 'Project'
   };
 };
 
 const mdtp = dispatch => {
   return {
-    fetchUserProjects: id => dispatch(fetchUserProjects(id)),
-    updateProject: project => dispatch(updateProject(project)),
-    composeProject: data => dispatch(composeProject(data))
+    fetchUserElements: id => dispatch(fetchUserProjects(id)),
+    updateElement: project => dispatch(updateProject(project)),
+    composeElement: data => dispatch(composeProject(data))
   };
 };
 
-export default connect(mstp, mdtp)(ProjectsPage);
+export default connect(mstp, mdtp)(ProfileElement);
