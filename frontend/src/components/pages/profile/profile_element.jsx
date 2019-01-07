1b8ec08128b3b859ecf211528734b3ac82a494fa
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-07 15:57:12 -0800

    Add multiple messages to profile

--- frontend/src/components/pages/profile/profile_element.jsx
+++ frontend/src/components/pages/profile/profile_element.jsx
@@ -7,10 +7,11 @@ class ProfileElement extends React.Component {
     super(props);
 
     this.state = {
-      title: '',
-      description: '',
+      title: "",
+      description: "",
       elements: [],
-      errors: {}
+      errors: {},
+      msg: ""
     };
 
     this.label = `Create ${this.props.label}`;
@@ -31,9 +32,11 @@ class ProfileElement extends React.Component {
   }
 
   update(field) {
-    return e => this.setState({
-      [field]: e.currentTarget.value
-    });
+    return e =>
+      this.setState({
+        [field]: e.currentTarget.value,
+        msg: ""
+      });
   }
 
   handleSubmit(e) {
@@ -42,31 +45,53 @@ class ProfileElement extends React.Component {
       title: this.state.title,
       description: this.state.description
     };
-    this.props.composeElement(newElement)
+    this.props
+      .composeElement(newElement)
       // .then(res => console.log(res.errors));
       .then(res => {
         if (res.errors === undefined) {
-          this.setState({ title: '', description: '' });
+          this.setState({
+            title: "",
+            description: "",
+            msg: "Created Successfully"
+          });
           // this.props.fetchUserElements(this.props.user.id);
+        } else {
+          this.setState({ msg: "Please correct errors listed below" });
         }
+      })
+      .then(() => {
+        setTimeout(() => {
+          this.setState({ msg: "" });
+        }, 2000);
       });
+  }
+
+  renderMsg() {
+    if (this.state.msg === "") {
+      return <div></div>;
+    } else {
+      return (
+        <div className="msg-container">
+          <p>{this.state.msg}</p>
+        </div>
+      );
+    }
   }
 
   renderErrors() {
     // debugger
     if (Object.keys(this.state.errors).length === 0) {
-      return (
-        <div></div>
-      );
+      return <div />;
     } else {
       return (
         <div className="errors-container ib">
-        {/* <div> */}
+          {/* <div> */}
           <ul className="errors-list">
-          {/* <ul> */}
+            {/* <ul> */}
             {Object.keys(this.state.errors).map((error, i) => (
               // <li key={`error-${i}`}>
-                <li className="error-item" key={`error-${i}`}>
+              <li className="error-item" key={`error-${i}`}>
                 {this.state.errors[error]}
               </li>
             ))}
@@ -78,23 +103,32 @@ class ProfileElement extends React.Component {
 
   render() {
     if (this.state.elements === undefined) {
+      return <div className="no-grps" />;
+    } else {
       return (
-        <div className="no-grps"></div>
-      );
-    }
-    else {
-      return <div className="grps-page-wrapper">
+        <div className="grps-page-wrapper">
           <div className="all-elements-container">
             <div className="el-label">New {this.props.label}</div>
             <div className="input-background">
               <div className="element-form-wrapper">
                 <form onSubmit={this.handleSubmit}>
                   <div className="element-form">
-                    <input type="text" value={this.state.title} onChange={this.update("title")} placeholder="Title" />
+                    <input
+                      type="text"
+                      value={this.state.title}
+                      onChange={this.update("title")}
+                      placeholder="Title"
+                    />
                     <div className="elem-desc">
                       <hr />
-                      <textarea value={this.state.description} onChange={this.update("description")} placeholder="Description" />
-                      <button className="profile-submit-btn">{this.label}</button>
+                      <textarea
+                        value={this.state.description}
+                        onChange={this.update("description")}
+                        placeholder="Description"
+                      />
+                      <button className="profile-submit-btn">
+                        {this.label}
+                      </button>
                     </div>
                   </div>
                 </form>
@@ -111,7 +145,9 @@ class ProfileElement extends React.Component {
             />
           ))}
           {this.renderErrors()}
-        </div>;
+          {this.renderMsg()}
+        </div>
+      );
     }
   }
 }
