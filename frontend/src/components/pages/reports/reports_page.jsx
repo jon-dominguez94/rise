f6f7efe7bc0195492f157a16898dfc5d66ed854f
Sameeh <sameehkhan1@gmail.com>
2019-01-07 23:37:47 -0800

    Complete title and render of the entries for specific weeks

--- frontend/src/components/pages/reports/reports_page.jsx
+++ frontend/src/components/pages/reports/reports_page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../../css/report_page.css';
 
 class ReportsPage extends React.Component {
   constructor(props) {
@@ -14,17 +15,20 @@ class ReportsPage extends React.Component {
     if (this.props.report === undefined || this.props.entries === undefined) return <div></div>
     // debugger
     return (
-      <div>
-
-
+      <div className='report-page-container'>
+        <div className='report-page-title'>Week {this.props.report.week} Entries</div>
+        <div className='all-entries-div'>
         {this.props.entries.map(entry => {
           return (
-            <div key={entry.id}>
+            // <div>
+              <div className='report-entry' key={entry.id}>
               {entry.description}
-            </div>
+              </div>
+            // </div>
           )
         })}
-      
+        </div>
+
       </div>
       // <div>{this.props.report.week}</div>
     );
