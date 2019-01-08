131b97810d339496d07f7d932609d1d7462e0a01
Mark Kopec <mark.kopec@gmail.com>
2019-01-08 11:04:46 -0800

    Use default template for email verification

--- routes/api/users.js
+++ routes/api/users.js
@@ -66,9 +66,10 @@ router.post('/register', (req, res) => {
             
             var params = {
               EmailAddress: req.body.email,
-              TemplateName: "RiseEmailTemplate"
+              // TemplateName: "RiseEmailTemplate"
             };
-            ses.sendCustomVerificationEmail(params, function (err, data) {
+            // ses.sendCustomVerificationEmail(params, function (err, data) {
+            ses.verifyEmailIdentity(params, function (err, data) {
               if (err) console.log(err, err.stack);
               // an error occurred
               else console.log(data); // successful response
