589a2d406a36e0f4e007217d7cc9df273f4d6ddb
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-08 14:50:03 -0800

    Simplify navbar

--- frontend/src/components/nav/home_links.jsx
+++ frontend/src/components/nav/home_links.jsx
@@ -1,16 +1,54 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const HomeLinks = (props) => (
+const HomeLinks = props => (
   <div className="navbar-links">
     <span className="bar-link user-greet">Hello, {props.user.fname}!</span>
     <hr />
-    <NavLink className="bar-link" to={'/profile'} activeClassName="selected">Profile</NavLink>
-    <NavLink className="bar-link" to={'/reports'} activeClassName="selected">Reports</NavLink>
-    <NavLink className="bar-link" to={'/reminders'} activeClassName="selected">Reminders</NavLink>
+    {/* <NavLink className="bar-link" to={'/profile'} activeClassName="selected">Profile</NavLink> */}
+
+    <NavLink
+      className="bar-link"
+      to={"/profile/projects"}
+      activeClassName="selected"
+    >
+      Projects
+    </NavLink>
+    <NavLink
+      className="bar-link"
+      to={"/profile/goals"}
+      activeClassName="selected"
+    >
+      Goals
+    </NavLink>
+    <NavLink
+      className="bar-link"
+      to={"/profile/roles"}
+      activeClassName="selected"
+    >
+      Roles
+    </NavLink>
+    
+    <NavLink className="bar-link" to={"/reports"} activeClassName="selected">
+      Reports
+    </NavLink>
+    <NavLink className="bar-link" to={"/reminders"} activeClassName="selected">
+      Reminders
+    </NavLink>
     <hr />
-    <NavLink className="bar-link" to={'/home'} activeClassName="selected">Home</NavLink>
-    <button className="bar-link" onClick={() => props.logout()}>Sign Out</button>
+    <NavLink className="bar-link" to={"/home"} activeClassName="selected">
+      Home
+    </NavLink>
+    <NavLink
+      className="bar-link"
+      to={"/profile/account"}
+      activeClassName="selected"
+    >
+      Account Info
+    </NavLink>
+    <button className="bar-link" onClick={() => props.logout()}>
+      Sign Out
+    </button>
   </div>
 );
 
