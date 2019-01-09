68bb48f562ad8c2a0457a8e0a8b9c433dbe989d9
TedWildenradt <tedkwildenradt@gmail.com>
2019-01-09 08:50:28 -0800

    Footer component

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -5,6 +5,8 @@ import ReportPhoto from '../../reports_instructions.jpg'
 import ProjectPhoto from '../../project_instruction2.jpg'
 import RolePhoto from '../../roles_instructions.jpg'
 import ReminderPhoto from '../../reminders_instructions.jpg'
+import Footer from './footer';
+import { NavLink } from 'react-router-dom';
 
 class MainPage extends React.Component {
   render() {
@@ -20,14 +22,14 @@ class MainPage extends React.Component {
             <img src={ReportPhoto} alt=""/>
           </div>
           <div className="reports-instructions instructions" id="odd1">
-            <h2>Reports</h2>
+            <NavLink to={"/reports"}><h2>Reports</h2></NavLink>
             <p>Create entries for your weekly and monthly Achievements!</p>
           </div>
         </div>
 
         <div className="goals-instructions-container container">
           <div className="goals-instructions instructions" id="even1">
-            <h2>Goals</h2>
+            <NavLink to={"profile/goals"}><h2>Goals</h2></NavLink>
             <p>Your goals matter to you. Record them so you can track your progress!</p>
           </div>
           <div className="goals-photo photo">
@@ -40,14 +42,14 @@ class MainPage extends React.Component {
             <img src={RolePhoto} alt=""/>
           </div>
           <div className="roles-instructions instructions" id="odd2">
-            <h2>Roles</h2>
+            <NavLink to={"profile/roles"}><h2>Roles</h2></NavLink>
             <p>Track your varying roles throughout the year.</p>
           </div>
         </div>
 
         <div className="projects-instructions-container container">
           <div className="projects-instructions instructions" id="even2">
-            <h2>Projects</h2>
+            <NavLink to={"profile/projects"}><h2>Projects</h2></NavLink>
             <p>Keep a record of all the projects you have completed or worked on.</p>
           </div>
           <div className="projects-photo photo">
@@ -60,11 +62,16 @@ class MainPage extends React.Component {
             <img src={ReminderPhoto} alt=""/>
           </div>
           <div className="reminders-instructions instructions" id="odd3">
-            <h2>Reminders</h2>
+            <NavLink to={"/reminders"}><h2>Reminders</h2></NavLink>
             <p>We know life is hectic. Let us take care of the weekly reminders so you can stay focused!</p>
           </div>
         </div>
+        <div className="space-break">
 
+        </div>
+        <div>
+          <Footer />
+        </div>
       </div>
     );
   }
