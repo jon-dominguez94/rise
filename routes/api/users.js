cd053c7f2acc1712f533a2328d8de2dc1f5607ef
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-31 11:21:18 -0800

    Reset exp time to one hour

--- routes/api/users.js
+++ routes/api/users.js
@@ -161,7 +161,7 @@ router.post('/login', (req, res) => {
         jwt.sign(
           payload,
           keys.secretOrKey,
-          { expiresIn: 60 },
+          { expiresIn: 3600 },
           (err, token) => {
             res.json({
               success: true,
